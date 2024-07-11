2c6b7bfbb076f922c39653cbf56c7e3f2baa8409
oemuraye <oemuraye@gmail.com>
2024-07-11 04:20:21 +0100

    background img fixing

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@
 
 	"use strict";
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+// 	$(window).stellar({
+//     responsive: true,
+//     parallaxBackgrounds: true,
+//     parallaxElements: true,
+//     horizontalScrolling: false,
+//     hideDistantElements: false,
+//     scrollProperty: 'scroll'
+//   });
 
 
 	var fullHeight = function() {
